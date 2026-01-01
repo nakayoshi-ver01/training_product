7cc2df9b97bcae35640c84c74d82416e2f0862a9
nakayoshi-ver01 <nakayoshiver01@gmail.com>
2026-01-01 20:48:14 +0900

    重み付き3目並べにする

--- webgame/urls.py
+++ webgame/urls.py
@@ -2,4 +2,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('game.urls')),
+    path('tic-tac-toe/', include('game.urls')),
 ]
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
